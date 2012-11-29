24fb76355543beb3c44b4d66e751ffbb7207a7b7
Sho Hiroto <hiroto@val.co.jp>
2012-11-29 17:26:28 +0900

    ２倍表示機能を追加

--- SunVisor/main.cpp
+++ SunVisor/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, const char * argv[])
     cout << "数値を入力して下さい。" << endl;
     int num;
     cin >> num;
+    cout << num << " × 2 = " << num*2 << endl;
     cout << num << " × 3 = " << num*3 << endl;
     return 0;
 }
