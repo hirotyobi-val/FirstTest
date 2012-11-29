99bb739073882d76d992709fbec6b696acfe97a7
Sho Hiroto <hiroto@val.co.jp>
2012-11-29 15:56:22 +0900

    first commit

---
+++ SunVisor/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    cout << "数値を入力して下さい。" << endl;
+    int num;
+    cin >> num;
+    cout << num << " × 3 = " << num*3 << endl;
+    return 0;
+}
+
